85f42e8732133786d4bf334bab6f43caa4cd7118
Zizon <Administrator@PC-03129.(none)>
2011-06-28 10:48:45 +0800

    remove log

--- src/main/java/org/apache/hadoop/mapred/RoundRobinScheduler.java
+++ src/main/java/org/apache/hadoop/mapred/RoundRobinScheduler.java
@@ -3,7 +3,6 @@ package org.apache.hadoop.mapred;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -25,8 +24,10 @@ import org.apache.commons.logging.LogFactory;
  */
 public class RoundRobinScheduler extends TaskScheduler {
 
-	private static final Log LOGGER = LogFactory.getLog(RoundRobinScheduler.class);
-	private static ExecutorService SERVICE = new ThreadPoolExecutor(0, Short.MAX_VALUE, 10, TimeUnit.SECONDS,
+	private static final Log LOGGER = LogFactory
+			.getLog(RoundRobinScheduler.class);
+	private static ExecutorService SERVICE = new ThreadPoolExecutor(0,
+			Short.MAX_VALUE, 10, TimeUnit.SECONDS,
 			new LinkedBlockingQueue<Runnable>());
 
 	private Map<JobInProgress, JobInProgress> jobs = new ConcurrentHashMap<JobInProgress, JobInProgress>();
@@ -43,31 +44,35 @@ public class RoundRobinScheduler extends TaskScheduler {
 		super.start();
 		this.tracker = 0;
 		RoundRobinScheduler.LOGGER.info("start round robin scheduler");
-		this.taskTrackerManager.addJobInProgressListener(new JobInProgressListener() {
-			@Override
-			public void jobUpdated(JobChangeEvent event) {
-				RoundRobinScheduler.LOGGER.info("get an event:" + event);
-			}
+		this.taskTrackerManager
+				.addJobInProgressListener(new JobInProgressListener() {
+					@Override
+					public void jobUpdated(JobChangeEvent event) {
+						RoundRobinScheduler.LOGGER
+								.info("get an event:" + event);
+					}
 
-			@Override
-			public void jobRemoved(JobInProgress job) {
-				RoundRobinScheduler.LOGGER.info("remove job	" + job);
-				RoundRobinScheduler.this.jobs.remove(job);
-			}
+					@Override
+					public void jobRemoved(JobInProgress job) {
+						RoundRobinScheduler.LOGGER.info("remove job	" + job);
+						RoundRobinScheduler.this.jobs.remove(job);
+					}
 
-			@Override
-			public void jobAdded(final JobInProgress job) throws IOException {
-				RoundRobinScheduler.LOGGER.info("add job	" + job);
-				if (job != null) {
-					RoundRobinScheduler.SERVICE.execute(new Runnable() {
-						public void run() {
-							RoundRobinScheduler.this.taskTrackerManager.initJob(job);
-							RoundRobinScheduler.this.jobs.put(job, job);
+					@Override
+					public void jobAdded(final JobInProgress job)
+							throws IOException {
+						RoundRobinScheduler.LOGGER.info("add job	" + job);
+						if (job != null) {
+							RoundRobinScheduler.SERVICE.execute(new Runnable() {
+								public void run() {
+									RoundRobinScheduler.this.taskTrackerManager
+											.initJob(job);
+									RoundRobinScheduler.this.jobs.put(job, job);
+								}
+							});
 						}
-					});
-				}
-			}
-		});
+					}
+				});
 	}
 
 	/**
@@ -75,7 +80,8 @@ public class RoundRobinScheduler extends TaskScheduler {
 	 */
 	@Override
 	public List<Task> assignTasks(TaskTrackerStatus status) throws IOException {
-		Iterator<Entry<JobInProgress, JobInProgress>> round_robin = this.jobs.entrySet().iterator();
+		Iterator<Entry<JobInProgress, JobInProgress>> round_robin = this.jobs
+				.entrySet().iterator();
 		// nothing to do
 		if (!round_robin.hasNext()) {
 			return new ArrayList<Task>(0);
@@ -85,13 +91,11 @@ public class RoundRobinScheduler extends TaskScheduler {
 		List<JobInProgress> in_progress = new ArrayList<JobInProgress>();
 		while (round_robin.hasNext()) {
 			JobInProgress job = round_robin.next().getKey();
-			if (job != null && job.getStatus().getRunState() == JobStatus.RUNNING) {
+			if (job != null
+					&& job.getStatus().getRunState() == JobStatus.RUNNING) {
 				in_progress.add(job);
 			}
 		}
-
-		// shuffle the jobs, avoid starvation
-		Collections.shuffle(in_progress);
 
 		// no runnning job
 		if (in_progress.size() <= 0) {
@@ -100,7 +104,8 @@ public class RoundRobinScheduler extends TaskScheduler {
 
 		RoundRobinScheduler.LOGGER.info("assign tasks for " + status);
 		final List<Task> assigned = new ArrayList<Task>();
-		final int task_tracker = this.taskTrackerManager.getClusterStatus().getTaskTrackers();
+		final int task_tracker = this.taskTrackerManager.getClusterStatus()
+				.getTaskTrackers();
 		final int uniq_hosts = this.taskTrackerManager.getNumberOfUniqueHosts();
 
 		// assign map task
@@ -108,42 +113,37 @@ public class RoundRobinScheduler extends TaskScheduler {
 
 		// ensure not empty
 		while (map_capacity > 0) {
-			RoundRobinScheduler.LOGGER.info("map capacity:" + map_capacity);
-
 			// iterate it
 			this.tracker = ++this.tracker % in_progress.size();
-			JobInProgress job = in_progress.get(this.tracker);
-			Task task = job.obtainNewMapTask(status, task_tracker, uniq_hosts);
+			Task task = in_progress.get(this.tracker).obtainNewMapTask(status,
+					task_tracker, uniq_hosts);
 			if (task != null) {
-				RoundRobinScheduler.LOGGER.info("add a map task:" + task);
 				assigned.add(task);
 				map_capacity--;
 			} else {
-				RoundRobinScheduler.LOGGER.info("no map task for node");
 				break;
 			}
 		}
 
 		// assign reduce task
-		int reduce_capacity = status.getMaxReduceTasks() - status.countReduceTasks();
+		int reduce_capacity = status.getMaxReduceTasks()
+				- status.countReduceTasks();
 		while (reduce_capacity > 0) {
-			RoundRobinScheduler.LOGGER.info("reduce capacity:" + map_capacity);
-
 			// iterate it
 			this.tracker = ++this.tracker % in_progress.size();
-			JobInProgress job = in_progress.get(this.tracker);
-			Task task = job.obtainNewReduceTask(status, task_tracker, uniq_hosts);
+			Task task = in_progress.get(this.tracker).obtainNewReduceTask(
+					status, task_tracker, uniq_hosts);
 			if (task != null) {
-				RoundRobinScheduler.LOGGER.info("add a reduce task:" + task);
 				assigned.add(task);
 				reduce_capacity--;
 			} else {
-				RoundRobinScheduler.LOGGER.info("no reduce task for node");
 				break;
 			}
 		}
 
-		RoundRobinScheduler.LOGGER.info("assign task:" + assigned.size());
+		RoundRobinScheduler.LOGGER.info("assigned task:" + assigned.size()
+				+ " map_capacity:" + map_capacity + " reduce_capacity:"
+				+ reduce_capacity);
 		return assigned;
 	}
 
